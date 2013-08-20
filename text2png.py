735e633df270249aab84b2fe8cc15806ade77a51
Zhaohan Jia <stone8936@hotmail.com>
2013-08-20 16:43:58 +0200

    pillow py3

--- text2png.py
+++ text2png.py
@@ -4,9 +4,10 @@ from PIL import Image
 from PIL import ImageDraw
 
 # font = ImageFont.truetype("Arial-Bold.ttf",14)
-font = ImageFont.truetype("arial.ttf",14)
-img=Image.new("RGBA", (500,250),(255,255,255))
+font = ImageFont.truetype("SIXTY.TTF",250)
+img=Image.new("RGB", (250,250),(255,255,255))
+#img=Image.new("R")
 draw = ImageDraw.Draw(img)
-draw.text((0, 0),"This is a test",(0,0,0),font=font)
+draw.text((0, 0),"B!",(0,0,0),font=font)
 draw = ImageDraw.Draw(img)
-img.save("a_test.png")
+img.save("a_test.jpg")
